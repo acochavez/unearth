bcb3869ab076a0a7936b37343ef2f87b9112d288
Arisa Ochavez <acochavez@voyagerinnovation.com>
2014-08-17 12:15:58 +0800

    Connect scripts

--- bin/unearth.js
+++ bin/unearth.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
-var cli = require('../lib/cli');
+var unearth = require('../lib/index');
 
-//Process command line arguments
-var options = cli.parse(process.argv);
+start = function () {
+  if (!process.argv[2]) {
+    return console.log('File is missing');
+  }
 
-//Run
+  index(process.argv[2], function(error, result){
+    if (error)
+      return console.log('Error Unearthing...');
+
+    console.log('Successfully Unearthed App...');
+  });
+}
+
+start();
