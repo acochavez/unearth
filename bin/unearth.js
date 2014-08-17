113d764475432f8e627f74ca328b96318cb904a6
Arisa Ochavez <acochavez@voyagerinnovation.com>
2014-08-17 12:23:35 +0800

    Get files

--- bin/unearth.js
+++ bin/unearth.js
@@ -1,19 +1,18 @@
-#!/usr/bin/env node
 'use strict';
 
-var unearth = require('../lib/index');
+var index = require('../lib/index');
 
-start = function () {
+var start = function () {
   if (!process.argv[2]) {
     return console.log('File is missing');
   }
 
-  index(process.argv[2], function(error, result){
+  index.unearthing(process.argv[2], function(error, result){
     if (error)
       return console.log('Error Unearthing...');
 
     console.log('Successfully Unearthed App...');
   });
-}
+};
 
 start();
